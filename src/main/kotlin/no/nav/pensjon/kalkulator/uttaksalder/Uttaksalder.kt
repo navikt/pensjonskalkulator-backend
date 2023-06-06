2e480fa50951fa5cc37d68de6dc343df2a15a931
Stephen Ramthun <stephenramthun@gmail.com>
2023-06-06 15:54:00 +0200

    Ignore uknown properties for uttaksalder

--- src/main/kotlin/no/nav/pensjon/kalkulator/uttaksalder/Uttaksalder.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/uttaksalder/Uttaksalder.kt
@@ -1,3 +1,6 @@
 package no.nav.pensjon.kalkulator.uttaksalder
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Uttaksalder(val aar: Int, val maaned: Int)
