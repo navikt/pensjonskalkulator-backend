0db52db032a77f63fda0912c498a230a57b86e26
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2023-08-18 11:56:54 +0200

    Add field description to UttaksperiodeSpec
    Also attempt to fix CodeQL failure

--- src/main/kotlin/no/nav/pensjon/kalkulator/avtale/client/np/UttaksperiodeSpec.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/avtale/client/np/UttaksperiodeSpec.kt
@@ -3,10 +3,17 @@ package no.nav.pensjon.kalkulator.avtale.client.np
 import no.nav.pensjon.kalkulator.avtale.Alder
 import no.nav.pensjon.kalkulator.avtale.Uttaksgrad
 
+/**
+ * Beskrivelse av felter:
+ * start: Må resultere i en dato tidligst inneværende måned kallet blir gjort.
+ *        Månedsverdien i startalder er relativ til, og etter, brukerens fødselsmåned.
+ *        F.eks., for en bruker født i april (4), så skal startmåned 2 tolkes som førstkommende juni (6 = 4 + 2) etter at brukeren har fylt startalder.
+ *        Dette kan bety en reell dato i året etter at brukeren har fylt startalder.
+ * grad: Uttaksgrad; i den siste uttaksperioden må grad være 100.
+ * aarligInntekt: Årlig inntekt i uttaksperioden.
+ */
 data class UttaksperiodeSpec(
     val start: Alder,
-    //val startAlder: Int,
-    //val startMaaned: Int,
     val grad: Uttaksgrad,
     val aarligInntekt: Int
 )
