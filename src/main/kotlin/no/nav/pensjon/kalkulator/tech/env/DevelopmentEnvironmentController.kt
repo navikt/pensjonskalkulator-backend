dea219d9f92e6e0d42e87af186cfc81f196e5c73
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2023-09-01 11:41:52 +0200

    Use GitHub token permissions monitor
    Also clean DevelopmentEnvironmentController

--- src/main/kotlin/no/nav/pensjon/kalkulator/tech/env/DevelopmentEnvironmentController.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/tech/env/DevelopmentEnvironmentController.kt
@@ -1,13 +1,12 @@
 package no.nav.pensjon.kalkulator.tech.env
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("internal/env")
-class DevelopmentEnvironmentController(@Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}") val uri: String) {
+class DevelopmentEnvironmentController {
 
     @GetMapping("azure")
     fun azureEnvironment() = environmentVariable("AZURE_APP_CLIENT_SECRET")
