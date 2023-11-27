76d1326e6d1a4062deb9b0888b9f3fe4ee88e58b
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2023-11-27 13:29:29 +0100

    Remove avtaler_uten_start metric
    (it is not useful)

--- src/main/kotlin/no/nav/pensjon/kalkulator/avtale/client/np/v3/metric/NorskPensjonPensjonsavtaleMetrics.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/avtale/client/np/v3/metric/NorskPensjonPensjonsavtaleMetrics.kt
@@ -8,21 +8,10 @@ import no.nav.pensjon.kalkulator.tech.metric.Metrics
 object NorskPensjonPensjonsavtaleMetrics {
 
     private const val MAANEDER_METRIC_NAME = "avtaleperiodemaaneder"
-    private const val AVTALER_UTEN_START_METRIC_NAME = "avtaler_uten_start"
     private const val MANGLENDE_SLUTTMAANED_INDIKATOR = ">"
-    private const val DEFAULT_KATEGORI = "null"
 
     fun updateMetrics(envelope: EnvelopeDto) {
-        envelope.body?.pensjonsrettigheter?.pensjonsRettigheter?.let {
-            countAvtalerUtenStart(it)
-            countMaaneder(it)
-        }
-    }
-
-    private fun countAvtalerUtenStart(rettigheter: List<PensjonsrettighetDto>) {
-        rettigheter
-            .filter { it.startAlder == null }
-            .forEach(::countKategori)
+        envelope.body?.pensjonsrettigheter?.pensjonsRettigheter?.let(::countMaaneder)
     }
 
     private fun countMaaneder(rettigheter: List<PensjonsrettighetDto>) {
@@ -31,17 +20,9 @@ object NorskPensjonPensjonsavtaleMetrics {
             .forEach(this::countMaaneder)
     }
 
-    private fun countKategori(rettighet: PensjonsrettighetDto) {
-        Metrics.countEvent(AVTALER_UTEN_START_METRIC_NAME, "${kategori(rettighet)}-${underkategori(rettighet)}")
-    }
-
     private fun countMaaneder(periode: UtbetalingsperiodeDto) {
         Metrics.countEvent(MAANEDER_METRIC_NAME, "${periode.startMaaned}-${sluttMaaned(periode)}")
     }
-
-    private fun kategori(rettighet: PensjonsrettighetDto) = rettighet.kategori ?: DEFAULT_KATEGORI
-
-    private fun underkategori(rettighet: PensjonsrettighetDto) = rettighet.underkategori ?: DEFAULT_KATEGORI
 
     private fun sluttMaaned(periode: UtbetalingsperiodeDto) = periode.sluttMaaned ?: MANGLENDE_SLUTTMAANED_INDIKATOR
 }
