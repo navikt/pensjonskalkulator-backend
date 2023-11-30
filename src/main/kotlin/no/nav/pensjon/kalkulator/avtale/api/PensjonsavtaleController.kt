f7fde8f7e872f7d6e3ec4cd7101a407cc2f0814b
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2023-11-30 15:29:09 +0100

    PEK-237 Return mock avtaler for test user

--- src/main/kotlin/no/nav/pensjon/kalkulator/avtale/api/PensjonsavtaleController.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/avtale/api/PensjonsavtaleController.kt
@@ -12,6 +12,8 @@ import no.nav.pensjon.kalkulator.avtale.api.dto.PensjonsavtalerDto
 import no.nav.pensjon.kalkulator.avtale.api.map.PensjonsavtaleMapper.fromDto
 import no.nav.pensjon.kalkulator.avtale.api.map.PensjonsavtaleMapper.toDto
 import no.nav.pensjon.kalkulator.common.api.ControllerBase
+import no.nav.pensjon.kalkulator.tech.json.ObjectMapperConfiguration
+import no.nav.pensjon.kalkulator.tech.security.ingress.PidGetter
 import no.nav.pensjon.kalkulator.tech.trace.TraceAid
 import no.nav.pensjon.kalkulator.tech.web.EgressException
 import org.springframework.web.bind.annotation.*
@@ -20,7 +22,8 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("api")
 class PensjonsavtaleController(
     private val service: PensjonsavtaleService,
-    private val traceAid: TraceAid
+    private val traceAid: TraceAid,
+    private val tempPidGetter: PidGetter? = null
 ) : ControllerBase(traceAid) {
 
     private val log = KotlinLogging.logger {}
@@ -45,8 +48,11 @@ class PensjonsavtaleController(
     fun fetchAvtaler(@RequestBody spec: PensjonsavtaleIngressSpecDto): PensjonsavtalerDto {
         traceAid.begin()
         log.debug { "Request for pensjonsavtaler V1: $spec" }
+        val mockFnr = tempPidGetter?.pid()?.value
 
         return try {
+            if ("10836397849" == mockFnr) mockMangeAvtaler() else
+
             toDto(timed(service::fetchAvtaler, fromDto(spec), "pensjonsavtaler V1"))
                 .also { log.debug { "Pensjonsavtaler respons V1: $it" } }
         } catch (e: EgressException) {
@@ -60,5 +66,105 @@ class PensjonsavtaleController(
 
     private companion object {
         private const val ERROR_MESSAGE = "feil ved henting av pensjonsavtaler"
+
+        /**
+         * Temporary function for testing many pensjonsavtaler
+         */
+        private fun mockMangeAvtaler(): PensjonsavtalerDto =
+             ObjectMapperConfiguration().objectMapper().readValue(
+                """{
+  "avtaler": [
+    {
+      "produktbetegnelse": "Innskuddpensjon (2 perioder - gradert)",
+      "kategori": "UNKNOWN",
+      "startAar": 70,
+      "sluttAar": 75,
+      "utbetalingsperioder": [
+        {
+          "startAlder": { "aar": 70, "maaneder": 0 },
+          "sluttAlder": { "aar": 72, "maaneder": 11 },
+          "aarligUtbetaling": 100000,
+          "grad": 100
+        },
+        {
+          "startAlder": { "aar": 73, "maaneder": 0 },
+          "sluttAlder": { "aar": 74, "maaneder": 11 },
+          "aarligUtbetaling": 50000,
+          "grad": 100
+        }
+      ]
+    },
+    {
+      "produktbetegnelse": "Nordea Liv",
+      "kategori": "PRIVAT_TJENESTEPENSJON",
+      "startAar": 75,
+      "sluttAar": 75,
+      "utbetalingsperioder": [
+        {
+          "startAlder": { "aar": 75, "maaneder": 0 },
+          "sluttAlder": { "aar": 75, "maaneder": 11 },
+          "aarligUtbetaling": 12345,
+          "grad": 100
+        }
+      ]
+    },
+    {
+      "produktbetegnelse": "Gjensidige (Gjensidige avtale)",
+      "kategori": "PRIVAT_TJENESTEPENSJON",
+      "startAar": 77,
+      "sluttAar": 87,
+      "utbetalingsperioder": [
+        {
+          "startAlder": { "aar": 77, "maaneder": 2 },
+          "sluttAlder": { "aar": 87, "maaneder": 1 },
+          "aarligUtbetaling": 12000,
+          "grad": 100
+        }
+      ]
+    },
+    {
+      "produktbetegnelse": "Oslo Pensjonsforsikring (livsvarig eksempel)",
+      "kategori": "OFFENTLIG_TJENESTEPENSJON",
+      "startAar": 62,
+      "utbetalingsperioder": [
+        {
+          "startAlder": { "aar": 62, "maaneder": 0 },
+          "aarligUtbetaling": 23456,
+          "grad": 100
+        }
+      ]
+    },
+    {
+      "produktbetegnelse": "DNB",
+      "kategori": "INDIVIDUELL_ORDNING",
+      "startAar": 67,
+      "sluttAar": 77,
+      "utbetalingsperioder": [
+        {
+          "startAlder": { "aar": 67, "maaneder": 0 },
+          "sluttAlder": { "aar": 77, "maaneder": 11 },
+          "aarligUtbetaling": 37264,
+          "grad": 100
+        }
+      ]
+    },
+    {
+      "produktbetegnelse": "IPS",
+      "kategori": "INDIVIDUELL_ORDNING",
+      "startAar": 70,
+      "sluttAar": 75,
+      "utbetalingsperioder": [
+        {
+          "startAlder": { "aar": 70, "maaneder": 6 },
+          "sluttAlder": { "aar": 75, "maaneder": 5 },
+          "aarligUtbetaling": 41802,
+          "grad": 100
+        }
+      ]
+    }
+  ],
+  "utilgjengeligeSelskap": []
+}""", PensjonsavtalerDto::class.java
+            )
     }
 }
