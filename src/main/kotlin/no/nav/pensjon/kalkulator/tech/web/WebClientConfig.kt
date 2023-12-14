5df5bfa6039faf192ffe0d25ed18b96f5469d4cf
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2023-12-14 23:26:30 +0100

    Bump deploy action to v2
    Also remove wiretap

--- src/main/kotlin/no/nav/pensjon/kalkulator/tech/web/WebClientConfig.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/tech/web/WebClientConfig.kt
@@ -1,7 +1,7 @@
 package no.nav.pensjon.kalkulator.tech.web
 
 import io.netty.channel.ChannelOption
-import io.netty.handler.logging.LogLevel
+//import io.netty.handler.logging.LogLevel
 import io.netty.handler.timeout.ReadTimeoutHandler
 import io.netty.handler.timeout.WriteTimeoutHandler
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer
@@ -19,7 +19,7 @@ import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 import reactor.netty.Connection
 import reactor.netty.http.client.HttpClient
-import reactor.netty.transport.logging.AdvancedByteBufFormat
+//import reactor.netty.transport.logging.AdvancedByteBufFormat
 import java.time.Duration
 import java.util.concurrent.TimeUnit
 
@@ -34,11 +34,11 @@ class WebClientConfig : WebClientCustomizer {
                         .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT.toInt())
                         .responseTimeout(Duration.ofMillis(TIMEOUT))
                         .doOnConnected(::addTimeoutHandlers)
-                        .wiretap(
-                            "reactor.netty.http.client.HttpClient",
-                            LogLevel.DEBUG,
-                            AdvancedByteBufFormat.TEXTUAL
-                        )
+                        //.wiretap(
+                        //    "reactor.netty.http.client.HttpClient",
+                        //    LogLevel.DEBUG,
+                        //    AdvancedByteBufFormat.TEXTUAL
+                        //)
                 )
             )
             .exchangeStrategies(largeBufferStrategies())
