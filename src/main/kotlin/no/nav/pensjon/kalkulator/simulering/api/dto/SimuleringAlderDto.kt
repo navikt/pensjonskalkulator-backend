3cd05a8ba4a10a68a06e0fe178d797d9380b1365
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2023-12-14 22:31:59 +0100

    Bump codeql, unleash, surefire
    Also delete unused code

--- src/main/kotlin/no/nav/pensjon/kalkulator/simulering/api/dto/SimuleringAlderDto.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/simulering/api/dto/SimuleringAlderDto.kt
@@ -6,10 +6,3 @@ data class SimuleringAlderDto(val aar: Int, val maaneder: Int) {
         require(maaneder in 0..11) { "0 <= maaneder <= 11" }
     }
 }
-
-data class SimuleringAlderV0Dto(val aar: Int, val maaned: Int) {
-    init {
-        require(aar in 0..200) { "0 <= aar <= 200" }
-        require(maaned in 0..12) { "0 <= maaned <= 12" }
-    }
-}
