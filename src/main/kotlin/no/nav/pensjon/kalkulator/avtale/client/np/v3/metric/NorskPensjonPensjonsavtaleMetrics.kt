d7d87b3c7205b6481a50f12707026c09992f7ee3
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2024-01-19 16:25:15 +0100

    Add avtaleperiodeantall metric

--- src/main/kotlin/no/nav/pensjon/kalkulator/avtale/client/np/v3/metric/NorskPensjonPensjonsavtaleMetrics.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/avtale/client/np/v3/metric/NorskPensjonPensjonsavtaleMetrics.kt
@@ -10,6 +10,7 @@ object NorskPensjonPensjonsavtaleMetrics {
     private const val KATEGORIER_EVENT_NAME = "avtalekategorier"
     private const val UNDERKATEGORIER_EVENT_NAME = "avtaleunderkategorier"
     private const val MAANEDER_EVENT_NAME = "avtaleperiodemaaneder"
+    private const val PERIODE_ANTALL_EVENT_NAME = "avtaleperiodeantall"
     private const val MANGLENDE_SLUTTMAANED_INDIKATOR = ">"
     private const val MANGLENDE_KATEGORI_INDIKATOR = "UDEFINERT"
 
@@ -20,6 +21,7 @@ object NorskPensjonPensjonsavtaleMetrics {
     private fun countInterestingEvents(rettigheter: List<PensjonsrettighetDto>) {
         countKategorier(rettigheter)
         countMaaneder(rettigheter)
+        countPeriodeAntall(rettigheter)
     }
 
     private fun countKategorier(rettigheter: List<PensjonsrettighetDto>) {
@@ -39,11 +41,21 @@ object NorskPensjonPensjonsavtaleMetrics {
     private fun countMaaneder(rettigheter: List<PensjonsrettighetDto>) {
         rettigheter
             .flatMap { it.utbetalingsperioder ?: emptyList() }
-            .forEach(this::countMaaneder)
+            .forEach(::countMaaneder)
+    }
+
+    private fun countPeriodeAntall(rettigheter: List<PensjonsrettighetDto>) {
+        rettigheter
+            .map { it.utbetalingsperioder?.size ?: 0 }
+            .forEach(::countPeriodeAntall)
     }
 
     private fun countMaaneder(periode: UtbetalingsperiodeDto) {
         countEvent(eventName = MAANEDER_EVENT_NAME, result = "${periode.startMaaned}-${sluttMaaned(periode)}")
+    }
+
+    private fun countPeriodeAntall(antall: Int) {
+        countEvent(eventName = PERIODE_ANTALL_EVENT_NAME, result = "$antall")
     }
 
     private fun sluttMaaned(periode: UtbetalingsperiodeDto) = periode.sluttMaaned ?: MANGLENDE_SLUTTMAANED_INDIKATOR
