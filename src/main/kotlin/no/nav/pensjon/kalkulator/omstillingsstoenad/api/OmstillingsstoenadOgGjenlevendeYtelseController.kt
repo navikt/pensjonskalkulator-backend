aca9a45a4b01b373f654ab155c14d386621c1be5
Anton Fofanov <anton.fofanov@nav.no>
2024-05-27 15:39:15 +0200

    PEK-403 oppdater OpenApiConfiguration, oppdater beskrivelse av endepunktet

--- src/main/kotlin/no/nav/pensjon/kalkulator/omstillingsstoenad/api/OmstillingsstoenadOgGjenlevendeYtelseController.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/omstillingsstoenad/api/OmstillingsstoenadOgGjenlevendeYtelseController.kt
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
-import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import no.nav.pensjon.kalkulator.common.api.ControllerBase
 import no.nav.pensjon.kalkulator.omstillingsstoenad.OmstillingOgGjenlevendeYtelseService
@@ -29,16 +28,16 @@ class OmstillingsstoenadOgGjenlevendeYtelseController(
     @GetMapping("/v1/loepende-omstillingsstoenad-eller-gjenlevendeytelse")
     @Operation(
         summary = "Mottar omstillingsstønad eller gjenlevende ytelse",
-        description = "Hvorvidt den innloggede brukeren mottar omstillingsstønad"
+        description = "Hvorvidt den innloggede brukeren mottar omstillingsstønad eller gjenlevende ytelse"
     )
     @ApiResponses(
         value = [
             ApiResponse(
                 responseCode = "200",
-                description = "Henting av omstillingsstønad utført."
+                description = "Henting av omstillingsstønad eller gjenlevende ytelse utført."
             ),
             ApiResponse(
-                responseCode = "503", description = "Henting av omstillingsstønad kunne ikke utføres av tekniske årsaker",
+                responseCode = "503", description = "Henting av omstillingsstønad eller gjenlevende ytelse kunne ikke utføres av tekniske årsaker",
                 content = [Content(examples = [ExampleObject(value = SERVICE_UNAVAILABLE_EXAMPLE)])]
             ),
         ]
