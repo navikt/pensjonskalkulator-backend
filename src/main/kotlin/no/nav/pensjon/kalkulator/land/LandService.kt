ba1cc1ebb6746911a38300e8d5910c95835c3709
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2024-08-27 12:05:53 +0200

    Exclude NOR from land-liste

--- src/main/kotlin/no/nav/pensjon/kalkulator/land/LandService.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/land/LandService.kt
@@ -6,13 +6,15 @@ import org.springframework.stereotype.Service
 class LandService {
 
     fun landListe(): List<LandInfo> =
-        Land.entries.map {
-            LandInfo(
-                landkode = it.name,
-                erAvtaleland = it.erAvtaleland,
-                bokmaalNavn = it.bokmaalNavn,
-                nynorskNavn = it.bokmaalNavn,
-                engelskNavn = it.engelskNavn
-            )
-        }
+        Land.entries
+            .filter { it != Land.NOR } // only 'utland' needed
+            .map {
+                LandInfo(
+                    landkode = it.name,
+                    erAvtaleland = it.erAvtaleland,
+                    bokmaalNavn = it.bokmaalNavn,
+                    nynorskNavn = it.bokmaalNavn,
+                    engelskNavn = it.engelskNavn
+                )
+            }
 }
