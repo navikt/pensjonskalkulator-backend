8c7023002c3512cacd723d4ef6d3204cd6755038
Anton Fofanov <anton.fofanov@nav.no>
2024-09-18 11:00:48 +0200

    PEK-585 legg til Mock for løpende vedtak

---
+++ src/main/kotlin/no/nav/pensjon/kalkulator/vedtak/api/LoependeVedtakDto.kt
@@ -0,0 +1,10 @@
+package no.nav.pensjon.kalkulator.vedtak.api
+
+data class LoependeVedtakDto(
+    val alderspensjon: LoependeSakDto?,
+    val ufoeretrygd: LoependeSakDto?,
+    val afpPrivat: LoependeSakDto?,
+    val afpOffentlig: LoependeSakDto?,
+)
+
+data class LoependeSakDto(val uttaksgrad: Int = 100)
---
+++ src/main/kotlin/no/nav/pensjon/kalkulator/vedtak/api/VedtakController.kt
@@ -0,0 +1,52 @@
+package no.nav.pensjon.kalkulator.vedtak.api
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import no.nav.pensjon.kalkulator.common.api.ControllerBase
+import no.nav.pensjon.kalkulator.common.api.ControllerBase.Companion.SERVICE_UNAVAILABLE_EXAMPLE
+import no.nav.pensjon.kalkulator.tech.trace.TraceAid
+import org.intellij.lang.annotations.Language
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api")
+class VedtakController(private val traceAid: TraceAid) : ControllerBase(traceAid) {
+
+    @GetMapping("/v1/vedtak/loepende-vedtak")
+    @Operation(
+        summary = "Har løpende saker",
+        description = "Hvorvidt den innloggede brukeren har løpende uføretrygd med uttaksgrad, alderspensjon med uttaksgrad, AFP privat eller offentlig"
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Sjekking av saker utført"
+            ),
+            ApiResponse(
+                responseCode = "503", description = "Sjekking av saker kunne ikke utføres av tekniske årsaker",
+                content = [Content(examples = [ExampleObject(value = SERVICE_UNAVAILABLE_EXAMPLE)])]
+            ),
+        ]
+    )
+    fun hentLoependeVedtak(): LoependeVedtakDto {
+        return LoependeVedtakDto(
+            alderspensjon = LoependeSakDto(60),
+            ufoeretrygd = LoependeSakDto(40),
+            afpPrivat = LoependeSakDto(),
+            afpOffentlig = null
+        )
+    }
+
+    override fun errorMessage() = ERROR_MESSAGE
+
+    private companion object {
+        private const val ERROR_MESSAGE = "Feil ved sjekking av løpende saker"
+    }
+
+}
