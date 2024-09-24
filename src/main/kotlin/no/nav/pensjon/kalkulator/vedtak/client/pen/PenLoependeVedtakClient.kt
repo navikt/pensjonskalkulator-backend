3ca4cf948716dcfa6a31fe21e2bd7bc28b9e306b
Anton Fofanov <anton.fofanov@nav.no>
2024-09-24 17:27:34 +0200

    PEK-585 bruk ny url for henting av løpende vedtak med grad

--- src/main/kotlin/no/nav/pensjon/kalkulator/vedtak/client/pen/PenLoependeVedtakClient.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/vedtak/client/pen/PenLoependeVedtakClient.kt
@@ -9,7 +9,6 @@ import no.nav.pensjon.kalkulator.vedtak.client.LoependeVedtakClient
 import no.nav.pensjon.kalkulator.vedtak.client.pen.dto.PenLoependeVedtakDto
 import no.nav.pensjon.kalkulator.vedtak.client.pen.map.LoependeVedtakMapper
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.core.ParameterizedTypeReference
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 
@@ -22,15 +21,18 @@ class PenLoependeVedtakClient(
 ) : PenClient(baseUrl, webClientBuilder, traceAid, retryAttempts), LoependeVedtakClient {
 
     override fun hentLoependeVedtak(pid: Pid): LoependeVedtak {
-        return doGet(
-            object : ParameterizedTypeReference<PenLoependeVedtakDto>() {},
-            path = PATH,
-            pid
-        )?.let(LoependeVedtakMapper::fromDto)
+        return webClient
+            .get()
+            .uri(PATH)
+            .header("fnr", pid.value)
+            .retrieve()
+            .bodyToMono(PenLoependeVedtakDto::class.java)
+            .block()
+            ?.let(LoependeVedtakMapper::fromDto)
             ?: throw EgressException("Kunne ikke hente loepende vedtak for brukeren")
     }
 
     private companion object {
-        private const val PATH = "vedtak/loependevedtak"
+        private const val PATH = "/api/simulering/vedtak/loependevedtak"
     }
 }
