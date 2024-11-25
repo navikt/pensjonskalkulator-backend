cc5acb075c364294186aa8e1d70a3c75342bce32
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2024-11-25 14:14:26 +0100

    Delete obsolete person-related code

--- src/main/kotlin/no/nav/pensjon/kalkulator/person/api/PersonController.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/person/api/PersonController.kt
@@ -13,9 +13,7 @@ import no.nav.pensjon.kalkulator.person.PersonService
 import no.nav.pensjon.kalkulator.person.api.dto.PersonResultV4
 import no.nav.pensjon.kalkulator.person.api.dto.PersonSpecV3
 import no.nav.pensjon.kalkulator.person.api.dto.PersonSpecV4
-import no.nav.pensjon.kalkulator.person.api.dto.PersonV2
 import no.nav.pensjon.kalkulator.person.api.dto.PersonV3
-import no.nav.pensjon.kalkulator.person.api.map.PersonMapperV2.dtoV2
 import no.nav.pensjon.kalkulator.person.api.map.PersonMapperV3
 import no.nav.pensjon.kalkulator.person.api.map.PersonMapperV4
 import no.nav.pensjon.kalkulator.tech.trace.TraceAid
@@ -32,44 +30,6 @@ class PersonController(
 ) : ControllerBase(traceAid) {
 
     private val log = KotlinLogging.logger {}
-
-    @GetMapping("v2/person")
-    @Operation(
-        summary = "Hent personinformasjon",
-        description = "Henter personinformasjon om den innloggede brukeren."
-    )
-    @ApiResponses(
-        value = [
-            ApiResponse(
-                responseCode = "200",
-                description = "Henting av personinformasjon utført."
-            ),
-            ApiResponse(
-                responseCode = "404",
-                description = "Personen ble ikke funnet."
-            ),
-            ApiResponse(
-                responseCode = "503",
-                description = "Henting av personinformasjon kunne ikke utføres av tekniske årsaker.",
-                content = [Content(examples = [ExampleObject(value = SERVICE_UNAVAILABLE_EXAMPLE)])]
-            ),
-        ]
-    )
-    fun personV2(): PersonV2 {
-        traceAid.begin()
-        log.debug { "Request for personinformasjon V2" }
-
-        return try {
-            dtoV2(timed(service::getPerson, "person"))
-                .also { log.debug { "Personinformasjon respons: $it" } }
-        } catch (e: NotFoundException) {
-            throw ResponseStatusException(HttpStatus.NOT_FOUND, e.message)
-        } catch (e: EgressException) {
-            handleError(e, "V2")!!
-        } finally {
-            traceAid.end()
-        }
-    }
 
     @PostMapping("v3/person")
     @Operation(
