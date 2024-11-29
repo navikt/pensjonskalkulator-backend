504167897acdd440e74ddd9c7da57dc1432b27f4
Anton Fofanov <anton.fofanov@nav.no>
2024-11-29 14:47:06 +0100

    PEK-716 bytt til MockitoBean

--- src/test/kotlin/no/nav/pensjon/kalkulator/tjenestepensjonsimulering/TjenestepensjonSimuleringControllerTest.kt
+++ src/test/kotlin/no/nav/pensjon/kalkulator/tjenestepensjonsimulering/TjenestepensjonSimuleringControllerTest.kt
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test
 import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
@@ -29,19 +29,19 @@ class TjenestepensjonSimuleringControllerTest {
     @Autowired
     private lateinit var mvc: MockMvc
 
-    @MockBean
+    @MockitoBean
     private lateinit var service: TjenestepensjonSimuleringService
 
-    @MockBean
+    @MockitoBean
     private lateinit var traceAid: TraceAid
 
-    @MockBean
+    @MockitoBean
     private lateinit var pidExtractor: PidExtractor
 
-    @MockBean
+    @MockitoBean
     private lateinit var groupMembershipService: GroupMembershipService
 
-    @MockBean
+    @MockitoBean
     private lateinit var auditor: Auditor
 
     @Test
