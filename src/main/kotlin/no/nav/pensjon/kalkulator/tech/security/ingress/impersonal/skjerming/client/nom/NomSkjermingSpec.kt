1ce88b0f03a38299c040dee2409199fbec2e0a5d
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2024-12-02 14:59:17 +0100

    PEK-829 Use POST in call to 'skjermet'

---
+++ src/main/kotlin/no/nav/pensjon/kalkulator/tech/security/ingress/impersonal/skjerming/client/nom/NomSkjermingSpec.kt
@@ -0,0 +1,6 @@
+package no.nav.pensjon.kalkulator.tech.security.ingress.impersonal.skjerming.client.nom
+
+/**
+ * NOM = Nav organisasjonsmaster
+ */
+data class NomSkjermingSpec(val personident: String)
