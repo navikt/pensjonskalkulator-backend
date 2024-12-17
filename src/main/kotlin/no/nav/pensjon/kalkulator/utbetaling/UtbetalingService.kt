1329b6e4b080f54f3636f6ad8f855a914d696aa0
Anton Fofanov <anton.fofanov@nav.no>
2024-12-17 10:54:52 +0100

    PEK-836 hent siste utbetaling skal feile i bakgrunn

--- src/main/kotlin/no/nav/pensjon/kalkulator/utbetaling/UtbetalingService.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/utbetaling/UtbetalingService.kt
@@ -2,6 +2,7 @@ package no.nav.pensjon.kalkulator.utbetaling
 
 import mu.KotlinLogging
 import no.nav.pensjon.kalkulator.tech.security.ingress.PidGetter
+import no.nav.pensjon.kalkulator.tech.web.EgressException
 import no.nav.pensjon.kalkulator.utbetaling.client.UtbetalingClient
 import org.springframework.stereotype.Component
 
@@ -13,7 +14,13 @@ class UtbetalingService(
     private val log = KotlinLogging.logger {}
 
     suspend fun hentSisteMaanedsUtbetaling(): SamletUtbetaling? {
-        val utbetalinger: List<Utbetaling> = utbetalingClient.hentSisteMaanedsUtbetaling(pidGetter.pid())
+        val utbetalinger: List<Utbetaling> = try{
+            utbetalingClient.hentSisteMaanedsUtbetaling(pidGetter.pid())
+        }
+        catch (e: EgressException){
+            log.warn(e) { "Klarte ikke å hente siste utbetaling: ${e.message}" }
+            return null
+        }
         log.info { "Hentet utbetalinger: $utbetalinger" }
 
         val alderspensjonUtbetalinger = utbetalinger.filter { it.gjelderAlderspensjon && it.beloep != null }
