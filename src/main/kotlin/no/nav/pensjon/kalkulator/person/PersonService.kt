1de01422ba01e8e4d41508399dcd739e5495c2c9
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-01-02 11:00:21 +0100

    Add temp. function to test 'økte aldersgrenser'

--- src/main/kotlin/no/nav/pensjon/kalkulator/person/PersonService.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/person/PersonService.kt
@@ -1,10 +1,12 @@
 package no.nav.pensjon.kalkulator.person
 
 import no.nav.pensjon.kalkulator.common.exception.NotFoundException
+import no.nav.pensjon.kalkulator.general.Alder
 import no.nav.pensjon.kalkulator.person.client.PersonClient
 import no.nav.pensjon.kalkulator.tech.metric.Metrics
 import no.nav.pensjon.kalkulator.tech.security.ingress.PidGetter
 import no.nav.pensjon.kalkulator.uttaksalder.normalder.NormertPensjoneringsalderService
+import no.nav.pensjon.kalkulator.uttaksalder.normalder.PensjoneringAldre
 import org.springframework.stereotype.Service
 import java.util.Collections.synchronizedMap
 
@@ -31,9 +33,20 @@ class PersonService(
             pid = pid.also(::validate),
             fetchFulltNavn = navnRequirement.needFulltNavn()
         )
-            ?.let { it.withPensjoneringAldre(normalderService.getAldre(it.foedselsdato)) }
+            //?.let { it.withPensjoneringAldre(normalderService.getAldre(it.foedselsdato)) }
+            ?.let { it.withPensjoneringAldre(pensjoneringAldre(it, pid)) }
             ?.also(::updateMetrics)
             ?: throw NotFoundException("person")
+
+    /**
+     * Temporary function to test 'økte aldersgrenser'.
+     */
+    private fun pensjoneringAldre(person: Person, pid: Pid): PensjoneringAldre =
+        when (pid.value) {
+            "11466909302" -> PensjoneringAldre(normalder = Alder(67, 4), nedreAldersgrense = Alder(62, 4))
+            "21437409932" -> PensjoneringAldre(normalder = Alder(68, 0), nedreAldersgrense = Alder(63, 0))
+            else -> normalderService.getAldre(person.foedselsdato)
+        }
 
     private fun validate(pid: Pid) {
         if (pid.isValid.not()) throw NotFoundException("person")
