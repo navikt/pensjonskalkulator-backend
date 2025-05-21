fae6e9d70b7e374796c7fed73bb09ca6ebcb63ab
martinstorvoll <martin.storvoll@nav.no>
2025-05-21 09:15:10 +0200

    Navnendring

--- src/main/kotlin/no/nav/pensjon/kalkulator/aldersgrense/AldersgrenseService.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/aldersgrense/AldersgrenseService.kt
@@ -1,10 +1,8 @@
 package no.nav.pensjon.kalkulator.aldersgrense
 
 import no.nav.pensjon.kalkulator.aldersgrense.api.dto.AldersgrenseSpec
-import no.nav.pensjon.kalkulator.general.Alder
-import no.nav.pensjon.kalkulator.person.api.dto.PersonAlderV4
-import no.nav.pensjon.kalkulator.person.api.dto.PersonPensjoneringAldreV4
 import no.nav.pensjon.kalkulator.uttaksalder.normalder.NormertPensjoneringsalderService
+import no.nav.pensjon.kalkulator.uttaksalder.normalder.PensjoneringAldre
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 
@@ -12,14 +10,9 @@ import java.time.LocalDate
 class AldersgrenseService(
     private val normertPensjoneringsalderService: NormertPensjoneringsalderService
 ) {
-    fun hentAldersgrenser(spec: AldersgrenseSpec): PersonPensjoneringAldreV4 {
+    fun hentAldersgrenser(spec: AldersgrenseSpec): PensjoneringAldre {
         val foedselsdato = LocalDate.of(spec.foedselsdato, 1, 1)
         val aldre = normertPensjoneringsalderService.getAldre(foedselsdato)
-        return PersonPensjoneringAldreV4(
-            normertPensjoneringsalder = aldre.normalder.toPersonAlderV4(),
-            nedreAldersgrense = aldre.nedreAldersgrense.toPersonAlderV4()
-        )
+        return aldre
     }
-
-    private fun Alder.toPersonAlderV4(): PersonAlderV4 = PersonAlderV4(aar, maaneder)
 }
--- src/main/kotlin/no/nav/pensjon/kalkulator/aldersgrense/api/map/AldersgrenseMapperV1.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/aldersgrense/api/map/AldersgrenseMapperV1.kt
@@ -2,12 +2,12 @@ package no.nav.pensjon.kalkulator.aldersgrense.api.map
 
 import no.nav.pensjon.kalkulator.aldersgrense.api.dto.AldersgrenseResultV1
 import no.nav.pensjon.kalkulator.aldersgrense.api.dto.PersonAlder
-import no.nav.pensjon.kalkulator.person.api.dto.PersonPensjoneringAldreV4
+import no.nav.pensjon.kalkulator.uttaksalder.normalder.PensjoneringAldre
 
 object AldersgrenseMapperV1 {
-    fun dtoV1(source: PersonPensjoneringAldreV4): AldersgrenseResultV1 =
+    fun dtoV1(source: PensjoneringAldre): AldersgrenseResultV1 =
         AldersgrenseResultV1(
-            normertPensjoneringsalder = PersonAlder(source.normertPensjoneringsalder.aar, source.normertPensjoneringsalder.maaneder),
+            normertPensjoneringsalder = PersonAlder(source.nedreAldersgrense.aar, source.normalder.maaneder),
             nedreAldersgrense = PersonAlder(source.nedreAldersgrense.aar, source.nedreAldersgrense.maaneder)
         )
 }
