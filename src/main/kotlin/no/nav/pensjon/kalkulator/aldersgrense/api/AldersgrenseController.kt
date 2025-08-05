c2a530533497d983218de727403d3470230d0f61
Anton Fofanov <anton.fofanov@nav.no>
2025-08-05 22:23:42 +0200

    PEK-1405 legg til v2/aldersgrense for uinnlogget

--- src/main/kotlin/no/nav/pensjon/kalkulator/aldersgrense/api/AldersgrenseController.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/aldersgrense/api/AldersgrenseController.kt
@@ -8,8 +8,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses
 import mu.KotlinLogging
 import no.nav.pensjon.kalkulator.aldersgrense.AldersgrenseService
 import no.nav.pensjon.kalkulator.aldersgrense.api.dto.AldersgrenseResultV1
+import no.nav.pensjon.kalkulator.aldersgrense.api.dto.AldersgrenseResultV2
 import no.nav.pensjon.kalkulator.aldersgrense.api.dto.AldersgrenseSpec
 import no.nav.pensjon.kalkulator.aldersgrense.api.map.AldersgrenseMapperV1
+import no.nav.pensjon.kalkulator.aldersgrense.api.map.AldersgrenseMapperV2
 import no.nav.pensjon.kalkulator.common.api.ControllerBase
 import no.nav.pensjon.kalkulator.common.exception.NotFoundException
 import no.nav.pensjon.kalkulator.tech.trace.TraceAid
@@ -59,6 +61,41 @@ class AldersgrenseController(
             traceAid.end()
         }
     }
+
+    @PostMapping("v2/aldersgrense")
+    @Operation(
+        summary = "Hent nedre aldersgrense, øvre aldersgrense og normert pensjonsalder",
+        description = "Henter informasjon om aldersgrensene for et årskull."
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Henting av aldersgrenser utført."
+            ),
+            ApiResponse(
+                responseCode = "503",
+                description = "Henting av aldersgrenser kunne ikke utføres av tekniske årsaker.",
+                content = [Content(examples = [ExampleObject(value = SERVICE_UNAVAILABLE_EXAMPLE)])]
+            ),
+        ]
+    )
+    fun aldersgrenserV2(@RequestBody spec: AldersgrenseSpec): AldersgrenseResultV2 {
+        traceAid.begin()
+        log.debug { "Request for aldersgrense V2" }
+
+        return try {
+            AldersgrenseMapperV2.dto(timed({ service.hentAldersgrenser(spec) }, "hentAldersgrenser"))
+                .also { log.debug { "aldersgrense respons: $it" } }
+        } catch (e: NotFoundException) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, e.message)
+        } catch (e: EgressException) {
+            handleError<AldersgrenseResultV2>(e, "V2")!!
+        } finally {
+            traceAid.end()
+        }
+    }
+
     override fun errorMessage() = ERROR_MESSAGE
 
     private companion object {
