aa48b1fca19853d969aedf93dd2ac633178dc1ec
Anton Fofanov <anton.fofanov@nav.no>
2025-08-05 22:55:53 +0200

    PEK-1405 cleanup

--- src/main/kotlin/no/nav/pensjon/kalkulator/aldersgrense/AldersgrenseService.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/aldersgrense/AldersgrenseService.kt
@@ -1,10 +1,8 @@
 package no.nav.pensjon.kalkulator.aldersgrense
 
 import no.nav.pensjon.kalkulator.aldersgrense.api.dto.AldersgrenseSpec
-import no.nav.pensjon.kalkulator.general.Alder
 import no.nav.pensjon.kalkulator.normalder.Aldersgrenser
 import no.nav.pensjon.kalkulator.normalder.NormertPensjonsalderService
-import no.nav.pensjon.kalkulator.normalder.VerdiStatus
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 
@@ -13,22 +11,8 @@ class AldersgrenseService(
     private val normertPensjonsalderService: NormertPensjonsalderService
 ) {
     fun hentAldersgrenser(spec: AldersgrenseSpec): Aldersgrenser {
-        if (System.getenv("NAIS_CLUSTER_NAME") == "dev-gcp") {
-            return mockedAldersgrenser()
-        }
-
         val foedselsdato = LocalDate.of(spec.foedselsdato, 1, 1)
         val aldre = normertPensjonsalderService.aldersgrenser(foedselsdato)
         return aldre
-    }
-
-    private fun mockedAldersgrenser(): Aldersgrenser {
-        return Aldersgrenser(
-            aarskull = 1963,
-            normalder = Alder(aar = 67, maaneder = 5),
-            nedreAlder = Alder(aar = 62, maaneder = 5),
-            oevreAlder = Alder(aar = 75, maaneder = 5),
-            verdiStatus = VerdiStatus.FAST
-        )
     }
 }
