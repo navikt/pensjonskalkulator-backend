c101a64a184cc7c4823833c40b36afa6295ad68b
martinstorvoll <martin.storvoll@nav.no>
2025-10-15 15:59:34 +0200

    Fjernet Trailing / TpTjenestepensjonClient Sender med tp ordninger i respons

--- src/main/kotlin/no/nav/pensjon/kalkulator/tjenestepensjonsimulering/TjenestepensjonSimuleringService.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/tjenestepensjonsimulering/TjenestepensjonSimuleringService.kt
@@ -2,6 +2,7 @@ package no.nav.pensjon.kalkulator.tjenestepensjonsimulering
 
 import no.nav.pensjon.kalkulator.ekskludering.EkskluderingFacade
 import no.nav.pensjon.kalkulator.tech.security.ingress.PidGetter
+import no.nav.pensjon.kalkulator.tjenestepensjon.client.tp.TpTjenestepensjonClient
 import no.nav.pensjon.kalkulator.tjenestepensjonsimulering.client.TjenestepensjonSimuleringClient
 import no.nav.pensjon.kalkulator.vedtak.LoependeVedtakService
 import org.springframework.stereotype.Service
@@ -11,22 +12,25 @@ class TjenestepensjonSimuleringService(
     private val pidGetter: PidGetter,
     private val tjenestepensjonSimuleringClient: TjenestepensjonSimuleringClient,
     private val loependeVedtakService: LoependeVedtakService,
-    private val ekskluderingFacade: EkskluderingFacade
+    private val ekskluderingFacade: EkskluderingFacade,
+    private val tpclient: TpTjenestepensjonClient
 ) {
     fun hentTjenestepensjonSimulering(spec: SimuleringOffentligTjenestepensjonSpec): OffentligTjenestepensjonSimuleringsresultat {
         val loependeVedtak = loependeVedtakService.hentLoependeVedtak()
         val erApoteker = ekskluderingFacade.apotekerEkskludering().ekskludert
         val erFoedtFoer1963 = spec.foedselsdato.year < 1963
         val harUfoeretrygdEllerPre2025OffentligAfp = loependeVedtak.ufoeretrygd != null || loependeVedtak.pre2025OffentligAfp != null
+        val pid = pidGetter.pid()
 
         if (erApoteker || (erFoedtFoer1963 && harUfoeretrygdEllerPre2025OffentligAfp)) {
-            return SPERRET_SIMULERINGSRESULTAT
+            val tpForhold = tpclient.tjenestepensjonsforhold(pid)
+            return OffentligTjenestepensjonSimuleringsresultat(
+                simuleringsResultatStatus = SimuleringsResultatStatus(ResultatType.IKKE_MEDLEM),
+                tpOrdninger = tpForhold.tpOrdninger
+            )
         }
 
-        return tjenestepensjonSimuleringClient.hentTjenestepensjonSimulering(request = spec, pid = pidGetter.pid())
+        return tjenestepensjonSimuleringClient.hentTjenestepensjonSimulering(request = spec, pid = pid)
     }
 
-    companion object {
-        private val SPERRET_SIMULERINGSRESULTAT = OffentligTjenestepensjonSimuleringsresultat(SimuleringsResultatStatus(ResultatType.IKKE_MEDLEM))
-    }
 }
