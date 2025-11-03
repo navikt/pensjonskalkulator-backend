b32405dcd4b109aebf4e80a43e57f79fac7659de
martinstorvoll <martin.storvoll@nav.no>
2025-11-03 22:56:53 +0100

    Open api config

--- src/main/kotlin/no/nav/pensjon/kalkulator/tech/api/OpenApiConfiguration.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/tech/api/OpenApiConfiguration.kt
@@ -62,6 +62,7 @@ class OpenApiConfiguration {
                 "/api/tpo-medlemskap",
                 "/api/v1/tpo-medlemskap",
                 "/api/v2/simuler-oftp",
+                "/api/v2/simuler-oftp/foer-1963",
                 "/api/status",
             )
             .build()
