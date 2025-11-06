49da797770f660b40e0e2608d980869e19f4f236
Espen Gjøstøl <50329374+espengjostol@users.noreply.github.com>
2025-11-06 15:59:09 +0100

    Use Kotest and Mockk in controller tests (#314)

--- src/test/kotlin/no/nav/pensjon/kalkulator/tech/toggle/api/FeatureToggleControllerTest.kt
+++ src/test/kotlin/no/nav/pensjon/kalkulator/tech/toggle/api/FeatureToggleControllerTest.kt
@@ -1,60 +1,72 @@
 package no.nav.pensjon.kalkulator.tech.toggle.api
 
+import com.ninjasquad.springmockk.MockkBean
+import io.kotest.core.spec.style.FunSpec
+import io.mockk.every
 import no.nav.pensjon.kalkulator.mock.MockSecurityConfiguration
+import no.nav.pensjon.kalkulator.mock.PersonFactory.pid
+import no.nav.pensjon.kalkulator.person.AdressebeskyttelseGradering
 import no.nav.pensjon.kalkulator.tech.security.ingress.PidExtractor
 import no.nav.pensjon.kalkulator.tech.security.ingress.impersonal.audit.Auditor
 import no.nav.pensjon.kalkulator.tech.security.ingress.impersonal.fortrolig.FortroligAdresseService
 import no.nav.pensjon.kalkulator.tech.security.ingress.impersonal.group.GroupMembershipService
 import no.nav.pensjon.kalkulator.tech.toggle.FeatureToggleService
 import no.nav.pensjon.kalkulator.tech.trace.TraceAid
-import org.junit.jupiter.api.Test
-import org.mockito.Mockito.`when`
+import org.intellij.lang.annotations.Language
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.context.annotation.Import
-import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @WebMvcTest(FeatureToggleController::class)
 @Import(MockSecurityConfiguration::class)
-class FeatureToggleControllerTest {
+class FeatureToggleControllerTest : FunSpec() {
 
     @Autowired
     private lateinit var mvc: MockMvc
 
-    @MockitoBean
+    @MockkBean
     private lateinit var featureToggleService: FeatureToggleService
 
-    @MockitoBean
+    @MockkBean(relaxed = true)
     private lateinit var traceAid: TraceAid
 
-    @MockitoBean
+    @MockkBean
     private lateinit var pidExtractor: PidExtractor
 
-    @MockitoBean
-    private lateinit var fortroligAdresseService: FortroligAdresseService
+    @MockkBean
+    private lateinit var adresseService: FortroligAdresseService
 
-    @MockitoBean
+    @MockkBean
     private lateinit var groupMembershipService: GroupMembershipService
 
-    @MockitoBean
+    @MockkBean
     private lateinit var auditor: Auditor
 
-    @Test
-    fun isEnabled() {
-        `when`(featureToggleService.isEnabled("feature1")).thenReturn(true)
+    init {
+        beforeSpec {
+            every { traceAid.begin() } returns Unit
+            every { pidExtractor.pid() } returns pid
+            every { adresseService.adressebeskyttelseGradering(any()) } returns AdressebeskyttelseGradering.UGRADERT
+            every { groupMembershipService.innloggetBrukerHarTilgang(any()) } returns true
+            every { auditor.audit(any(), any()) } returns Unit
+        }
 
-        mvc.perform(MockMvcRequestBuilders.get(URL))
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(MockMvcResultMatchers.content().json(RESPONSE_BODY))
+        test("is enabled") {
+            every { featureToggleService.isEnabled("feature1") } returns true
+
+            mvc.perform(MockMvcRequestBuilders.get(URL))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(RESPONSE_BODY))
+        }
     }
 
     private companion object {
-
         private const val URL = "/api/feature/feature1"
 
+        @Language("json")
         private const val RESPONSE_BODY = """{
         "enabled": true
     }"""
