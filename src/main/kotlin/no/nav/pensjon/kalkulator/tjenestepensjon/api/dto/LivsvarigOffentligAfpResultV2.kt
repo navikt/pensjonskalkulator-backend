2640c52f80b49469037e6788dca7f9ed6521a926
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-11-27 13:38:51 +0100

    PEK-1544 Fix JSON date format

--- src/main/kotlin/no/nav/pensjon/kalkulator/tjenestepensjon/api/dto/LivsvarigOffentligAfpResultV2.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/tjenestepensjon/api/dto/LivsvarigOffentligAfpResultV2.kt
@@ -1,13 +1,18 @@
 package no.nav.pensjon.kalkulator.tjenestepensjon.api.dto
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
 import java.time.LocalDate
 
 /**
  * Dataoverføringsobjekt (DTO) for innvilget livsvarig offentlig AFP i offentlig sektor.
  */
+@JsonInclude(NON_NULL)
 data class LivsvarigOffentligAfpResultV2(
     val afpStatus: Boolean?,
-    val virkningFom: LocalDate?,
+    @param:JsonFormat(shape = STRING, pattern = "yyyy-MM-dd") val virkningFom: LocalDate?,
     val maanedligBeloep: Int?,
     val sistBenyttetGrunnbeloep: Int?
 )
