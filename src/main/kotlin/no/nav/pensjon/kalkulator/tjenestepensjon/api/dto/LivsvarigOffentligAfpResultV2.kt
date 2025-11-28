5e6bded9c36d627910a96b43d1aa80e9810e5501
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-11-28 13:14:40 +0100

    PEK-1544 Include nulls in LivsvarigOffentligAfpResultV2

--- src/main/kotlin/no/nav/pensjon/kalkulator/tjenestepensjon/api/dto/LivsvarigOffentligAfpResultV2.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/tjenestepensjon/api/dto/LivsvarigOffentligAfpResultV2.kt
@@ -2,14 +2,11 @@ package no.nav.pensjon.kalkulator.tjenestepensjon.api.dto
 
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
 import java.time.LocalDate
 
 /**
  * Dataoverføringsobjekt (DTO) for innvilget livsvarig offentlig AFP i offentlig sektor.
  */
-@JsonInclude(NON_NULL)
 data class LivsvarigOffentligAfpResultV2(
     val afpStatus: Boolean?,
     @param:JsonFormat(shape = STRING, pattern = "yyyy-MM-dd") val virkningFom: LocalDate?,
