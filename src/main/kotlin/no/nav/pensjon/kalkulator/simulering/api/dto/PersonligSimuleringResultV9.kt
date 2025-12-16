38bf262db5a8566fcdb67f6588bf1746004d854b
martinstorvoll <martin.storvoll@nav.no>
2025-12-16 12:13:40 +0100

    Lagt til NotNull til flere felter

--- src/main/kotlin/no/nav/pensjon/kalkulator/simulering/api/dto/PersonligSimuleringResultV9.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/simulering/api/dto/PersonligSimuleringResultV9.kt
@@ -47,56 +47,56 @@ data class PersonligSimuleringAlderspensjonResultV9(
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class PersonligSimuleringPre2025OffentligAfpResultV9(
-    val alderAar: Int,
-    val totaltAfpBeloep: Int,
-    val tidligereArbeidsinntekt: Int,
-    val grunnbeloep: Int,
-    val sluttpoengtall: Double,
-    val trygdetid: Int,
-    val poengaarTom1991: Int,
-    val poengaarFom1992: Int,
-    val grunnpensjon: Int,
-    val tilleggspensjon: Int,
-    val afpTillegg: Int,
-    val saertillegg: Int,
-    val afpGrad: Int,
-    val afpAvkortetTil70Prosent: Boolean
+    @field:NotNull val alderAar: Int,
+    @field:NotNull val totaltAfpBeloep: Int,
+    @field:NotNull val tidligereArbeidsinntekt: Int,
+    @field:NotNull val grunnbeloep: Int,
+    @field:NotNull val sluttpoengtall: Double,
+    @field:NotNull val trygdetid: Int,
+    @field:NotNull val poengaarTom1991: Int,
+    @field:NotNull val poengaarFom1992: Int,
+    @field:NotNull val grunnpensjon: Int,
+    @field:NotNull val tilleggspensjon: Int,
+    @field:NotNull val afpTillegg: Int,
+    @field:NotNull val saertillegg: Int,
+    @field:NotNull val afpGrad: Int,
+    @field:NotNull val afpAvkortetTil70Prosent: Boolean
 )
 
 data class PersonligSimuleringAfpPrivatResultV9(
-    val alder: Int,
-    val beloep: Int,
-    val kompensasjonstillegg: Int,
-    val kronetillegg: Int,
-    val livsvarig: Int,
+    @field:NotNull val alder: Int,
+    @field:NotNull val beloep: Int,
+    @field:NotNull val kompensasjonstillegg: Int,
+    @field:NotNull val kronetillegg: Int,
+    @field:NotNull val livsvarig: Int,
     val maanedligBeloep: Int?
 )
 
 data class PersonligSimuleringAarligPensjonResultV9(
-    val alder: Int,
-    val beloep: Int,
-    val maanedligBeloep: Int?
+    @field:NotNull val alder: Int,
+    @field:NotNull val beloep: Int,
+    @field:NotNull val maanedligBeloep: Int?
 )
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class PersonligSimuleringVilkaarsproevingResultV9(
-    val vilkaarErOppfylt: Boolean,
+    @field:NotNull val vilkaarErOppfylt: Boolean,
     val alternativ: PersonligSimuleringAlternativResultV9?
 )
 
 data class PersonligSimuleringAarligInntektResultV9(
-    val aar: Int,
-    val pensjonsgivendeInntektBeloep: Int
+    @field:NotNull val aar: Int,
+    @field:NotNull val pensjonsgivendeInntektBeloep: Int
 )
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class PersonligSimuleringAlternativResultV9(
     val gradertUttaksalder: PersonligSimuleringAlderResultV9?,
     val uttaksgrad: Int?, // null implies 100 %
-    val heltUttaksalder: PersonligSimuleringAlderResultV9
+    @field:NotNull val heltUttaksalder: PersonligSimuleringAlderResultV9
 )
 
 data class PersonligSimuleringAlderResultV9(
-    val aar: Int,
-    val maaneder: Int
+    @field:NotNull val aar: Int,
+    @field:NotNull val maaneder: Int
 )
