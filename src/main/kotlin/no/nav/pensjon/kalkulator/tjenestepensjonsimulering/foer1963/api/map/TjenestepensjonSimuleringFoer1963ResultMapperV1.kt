eb1d053b5029cded997f642cdce2e75c62aee8af
martinstorvoll <martin.storvoll@nav.no>
2025-12-18 11:45:25 +0100

    Endret til V1

--- src/main/kotlin/no/nav/pensjon/kalkulator/tjenestepensjonsimulering/foer1963/api/map/TjenestepensjonSimuleringFoer1963ResultMapperV1.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/tjenestepensjonsimulering/foer1963/api/map/TjenestepensjonSimuleringFoer1963ResultMapperV1.kt
@@ -2,19 +2,16 @@ package no.nav.pensjon.kalkulator.tjenestepensjonsimulering.foer1963.api.map
 
 import no.nav.pensjon.kalkulator.tjenestepensjonsimulering.foer1963.Feilkode
 import no.nav.pensjon.kalkulator.tjenestepensjonsimulering.foer1963.OffentligTjenestepensjonSimuleringFoer1963Resultat
-import no.nav.pensjon.kalkulator.tjenestepensjonsimulering.foer1963.api.dto.OffentligTjenestepensjonSimuleringFoer1963ResultV2
-import no.nav.pensjon.kalkulator.tjenestepensjonsimulering.foer1963.api.dto.SimuleringsresultatFoer1963V2
-import no.nav.pensjon.kalkulator.tjenestepensjonsimulering.foer1963.api.dto.SimulertTjenestepensjonFoer1963V2
-import no.nav.pensjon.kalkulator.tjenestepensjonsimulering.foer1963.api.dto.UtbetalingsperiodeFoer1963V2
+import no.nav.pensjon.kalkulator.tjenestepensjonsimulering.foer1963.api.dto.*
 import no.nav.pensjon.kalkulator.tjenestepensjonsimulering.fra1963.api.dto.SimuleringsresultatStatusV2
 import kotlin.math.floor
 
 object TjenestepensjonSimuleringFoer1963ResultMapperV1 {
 
-    fun toDtoV2(resultat: OffentligTjenestepensjonSimuleringFoer1963Resultat): OffentligTjenestepensjonSimuleringFoer1963ResultV2 {
+    fun toDtoV2(resultat: OffentligTjenestepensjonSimuleringFoer1963Resultat): OffentligTjenestepensjonSimuleringFoer1963ResultV1 {
         val periods = resultat.utbetalingsperioder.map { utbetaling ->
 
-            UtbetalingsperiodeFoer1963V2(
+            UtbetalingsperiodeFoer1963V1(
                 startAlder = utbetaling.alderFom,
                 sluttAlder = utbetaling.alderTom,
                 aarligUtbetaling = utbetaling.arligUtbetaling?.let { floor(it).toInt() } ?: 0,
@@ -26,14 +23,14 @@ object TjenestepensjonSimuleringFoer1963ResultMapperV1 {
         }
 
         val simulert = if (resultat.tpnr != null && resultat.navnOrdning != null) {
-            SimulertTjenestepensjonFoer1963V2(
+            SimulertTjenestepensjonFoer1963V1(
                 tpLeverandoer = resultat.navnOrdning,
                 tpNummer = resultat.tpnr,
-                simuleringsresultat = SimuleringsresultatFoer1963V2(utbetalingsperioder = periods)
+                simuleringsresultat = SimuleringsresultatFoer1963V1(utbetalingsperioder = periods)
             )
         } else null
 
-        return OffentligTjenestepensjonSimuleringFoer1963ResultV2(
+        return OffentligTjenestepensjonSimuleringFoer1963ResultV1(
             simuleringsresultatStatus = resultat.feilkode?.let {
                 when (it) {
                     Feilkode.BRUKER_IKKE_MEDLEM_AV_TP_ORDNING -> SimuleringsresultatStatusV2.BRUKER_ER_IKKE_MEDLEM_AV_TP_ORDNING
