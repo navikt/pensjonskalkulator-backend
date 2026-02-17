317b690397f4cf6e6bed7ca9cdebdb6ef5b9cc16
Anton Fofanov <anton.fofanov@nav.no>
2026-02-17 17:50:38 +0100

    TPP-116 bruke tilgangsmaskinen for å hente tilgang til brukere

--- src/test/kotlin/no/nav/pensjon/kalkulator/tech/toggle/api/FeatureToggleControllerTest.kt
+++ src/test/kotlin/no/nav/pensjon/kalkulator/tech/toggle/api/FeatureToggleControllerTest.kt
@@ -9,8 +9,6 @@ import no.nav.pensjon.kalkulator.person.AdressebeskyttelseGradering
 import no.nav.pensjon.kalkulator.tech.security.ingress.PidExtractor
 import no.nav.pensjon.kalkulator.tech.security.ingress.impersonal.audit.Auditor
 import no.nav.pensjon.kalkulator.tech.security.ingress.impersonal.fortrolig.FortroligAdresseService
-import no.nav.pensjon.kalkulator.tech.security.ingress.impersonal.group.GroupMembershipService
-import no.nav.pensjon.kalkulator.tech.security.ingress.impersonal.tilgangsmaskinen.ShadowTilgangComparator
 import no.nav.pensjon.kalkulator.tech.toggle.FeatureToggleService
 import no.nav.pensjon.kalkulator.tech.trace.TraceAid
 import org.intellij.lang.annotations.Language
@@ -41,12 +39,6 @@ class FeatureToggleControllerTest : FunSpec() {
     private lateinit var adresseService: FortroligAdresseService
 
     @MockkBean
-    private lateinit var groupMembershipService: GroupMembershipService
-
-    @MockkBean(relaxed = true)
-    private lateinit var shadowTilgangComparator: ShadowTilgangComparator
-
-    @MockkBean
     private lateinit var auditor: Auditor
 
     init {
@@ -54,7 +46,6 @@ class FeatureToggleControllerTest : FunSpec() {
             every { traceAid.begin() } returns Unit
             every { pidExtractor.pid() } returns pid
             every { adresseService.adressebeskyttelseGradering(any()) } returns AdressebeskyttelseGradering.UGRADERT
-            every { groupMembershipService.innloggetBrukerHarTilgang(any()) } returns true
             every { auditor.audit(any(), any()) } returns Unit
         }
 
