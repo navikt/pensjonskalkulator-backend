732f2da9c19c0622e23964e162cb13bcd38dcdda
Anton Fofanov <anton.fofanov@nav.no>
2026-03-05 20:29:13 +0100

    TPP-194 bruk OBO-token for brevbestilling

--- src/main/kotlin/no/nav/pensjon/kalkulator/tech/api/OpenApiConfiguration.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/tech/api/OpenApiConfiguration.kt
@@ -58,6 +58,7 @@ class OpenApiConfiguration {
                 "/api/feature/**",
                 "/api/inntekt",
                 "/api/intern/v1/pensjon/simulering",
+                "/api/intern/v1/lagre-simulering",
                 "/api/sak-status",
                 "/api/tpo-medlemskap",
                 "/api/v1/tpo-medlemskap",
