f6a5af4fbda77f5a1a93d60b3b84f8b0b5d73431
Espen Gjøstøl <50329374+espengjostol@users.noreply.github.com>
2026-05-28 10:25:19 +0200

    TPP-244 Handle additional problem types (#457)

--- src/main/kotlin/no/nav/pensjon/kalkulator/simulering/api/v1/acl/result/SimuleringResult.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/simulering/api/v1/acl/result/SimuleringResult.kt
@@ -247,9 +247,14 @@ enum class SimuleringV1ProblemType(
     UGYLDIG_ANTALL_AAR(internalValue = ProblemType.UGYLDIG_ANTALL_AAR),
     UGYLDIG_PERSONIDENT(internalValue = ProblemType.UGYLDIG_PERSONIDENT),
     PERSON_IKKE_FUNNET(internalValue = ProblemType.PERSON_IKKE_FUNNET, httpStatus = HttpStatus.NOT_FOUND),
+    PERSON_FOR_LAV_ALDER(internalValue = ProblemType.PERSON_FOR_LAV_ALDER),
     PERSON_FOR_HOEY_ALDER(internalValue = ProblemType.PERSON_FOR_HOEY_ALDER),
+    UTILSTREKKELIG_INNTEKT(internalValue = ProblemType.UTILSTREKKELIG_INNTEKT, httpStatus = HttpStatus.OK),
     UTILSTREKKELIG_OPPTJENING(internalValue = ProblemType.UTILSTREKKELIG_OPPTJENING, httpStatus = HttpStatus.OK),
     UTILSTREKKELIG_TRYGDETID(internalValue = ProblemType.UTILSTREKKELIG_TRYGDETID, httpStatus = HttpStatus.OK),
     ANNEN_KLIENTFEIL(internalValue = ProblemType.ANNEN_KLIENTFEIL),
-    SERVERFEIL(internalValue = ProblemType.SERVERFEIL, httpStatus = HttpStatus.INTERNAL_SERVER_ERROR)
+    INTERN_DATA_INKONSISTENS(internalValue = ProblemType.INTERN_DATA_INKONSISTENS, httpStatus = HttpStatus.INTERNAL_SERVER_ERROR),
+    IMPLEMENTASJONSFEIL(internalValue = ProblemType.IMPLEMENTASJONSFEIL, httpStatus = HttpStatus.INTERNAL_SERVER_ERROR),
+    TREDJEPARTSFEIL(internalValue = ProblemType.TREDJEPARTSFEIL, httpStatus = HttpStatus.INTERNAL_SERVER_ERROR),
+    SERVERFEIL(internalValue = ProblemType.ANNEN_SERVERFEIL, httpStatus = HttpStatus.INTERNAL_SERVER_ERROR)
 }
--- src/main/kotlin/no/nav/pensjon/kalkulator/simulering/client/simulator/acl/result/ProblemTypeDto.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/simulering/client/simulator/acl/result/ProblemTypeDto.kt
@@ -4,6 +4,10 @@ import mu.KotlinLogging
 import no.nav.pensjon.kalkulator.validity.ProblemType
 import org.springframework.util.StringUtils.hasLength
 
+/**
+ * Corresponds to the following class in pensjonssimulator:
+ * no.nav.pensjon.simulator.api.nav.v2.acl.result.ProblemTypeDto
+ */
 enum class ProblemTypeDto(val internalValue: ProblemType) {
     UGYLDIG_UTTAKSDATO(internalValue = ProblemType.UGYLDIG_UTTAKSDATO),
     UGYLDIG_UTTAKSGRAD(internalValue = ProblemType.UGYLDIG_UTTAKSGRAD),
@@ -12,22 +16,26 @@ enum class ProblemTypeDto(val internalValue: ProblemType) {
     UGYLDIG_ANTALL_AAR(internalValue = ProblemType.UGYLDIG_ANTALL_AAR),
     UGYLDIG_PERSONIDENT(internalValue = ProblemType.UGYLDIG_PERSONIDENT),
     PERSON_IKKE_FUNNET(internalValue = ProblemType.PERSON_IKKE_FUNNET),
+    PERSON_FOR_LAV_ALDER(internalValue = ProblemType.PERSON_FOR_LAV_ALDER),
     PERSON_FOR_HOEY_ALDER(internalValue = ProblemType.PERSON_FOR_HOEY_ALDER),
+    UTILSTREKKELIG_INNTEKT(internalValue = ProblemType.UTILSTREKKELIG_INNTEKT),
     UTILSTREKKELIG_OPPTJENING(internalValue = ProblemType.UTILSTREKKELIG_OPPTJENING),
     UTILSTREKKELIG_TRYGDETID(internalValue = ProblemType.UTILSTREKKELIG_TRYGDETID),
     ANNEN_KLIENTFEIL(internalValue = ProblemType.ANNEN_KLIENTFEIL),
-    SERVERFEIL(internalValue = ProblemType.SERVERFEIL);
+    INTERN_DATA_INKONSISTENS(internalValue = ProblemType.INTERN_DATA_INKONSISTENS),
+    IMPLEMENTASJONSFEIL(internalValue = ProblemType.IMPLEMENTASJONSFEIL),
+    TREDJEPARTSFEIL(internalValue = ProblemType.TREDJEPARTSFEIL),
+    ANNEN_SERVERFEIL(internalValue = ProblemType.ANNEN_SERVERFEIL);
 
     companion object {
-        private val values = entries.toTypedArray()
-        private val defaultValue = SERVERFEIL
+        private val defaultValue = ANNEN_SERVERFEIL
         private val log = KotlinLogging.logger {}
 
         fun internalValue(value: String?) =
             fromExternalValue(value).internalValue
 
         private fun fromExternalValue(value: String?) =
-            values.singleOrNull { it.name.equals(value, true) } ?: default(value)
+            entries.singleOrNull { it.name.equals(value, true) } ?: default(value)
 
         private fun default(externalValue: String?) =
             if (hasLength(externalValue))
