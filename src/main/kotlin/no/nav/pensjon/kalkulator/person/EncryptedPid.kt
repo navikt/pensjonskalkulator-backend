440c82308e36da5d5f10862e1c3c8c43cb80054f
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2026-06-26 14:22:53 +0200

    TPP-510 Validate String argument in EncryptedPid

--- src/main/kotlin/no/nav/pensjon/kalkulator/person/EncryptedPid.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/person/EncryptedPid.kt
@@ -1,15 +1,15 @@
 package no.nav.pensjon.kalkulator.person
 
+import no.nav.pensjon.kalkulator.tech.crypto.EncryptionDetector.isEncryptedPid
+
 /**
  * Represents an encrypted person identifier.
  */
 data class EncryptedPid(val value: String) {
-    constructor(pid: PossiblyEncryptedPid) : this(validated(pid).value)
 
-    private companion object {
-        private fun validated(value: PossiblyEncryptedPid): PossiblyEncryptedPid {
-            require(value.isEncrypted) { "Expected encrypted PID" }
-            return value
-        }
+    constructor(pid: PossiblyEncryptedPid) : this(pid.value)
+
+    init {
+        require(isEncryptedPid(value)) { "Expected encrypted PID" }
     }
 }
