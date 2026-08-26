a7f05b8a6fbf75c16fbc091ebc6b386ed5636bce
martinstorvoll <martin.storvoll@nav.no>
2026-08-26 14:58:24 +0200

    Endre dto til skribenten

--- src/main/kotlin/no/nav/pensjon/kalkulator/lagring/LagreSimulering.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/lagring/LagreSimulering.kt
@@ -162,8 +162,7 @@ data class LagreMaanedligAlderspensjon(
     val kapittel20AndelTeller: Int?,
     val kapittel20Trygdetid: Int?,
     val garantipensjonBeloep: Int?,
-    val garantipensjonsnivaaBeloep: Int?,
-    val garantipensjonSats: Double?,
+    val garantipensjonSats: Int?,
     val garantitilleggBeloep: Int?,
     val grunnbeloep: Int?
 )
--- src/main/kotlin/no/nav/pensjon/kalkulator/lagring/api/dto/LagreSimuleringSpecDtoV1.kt
+++ src/main/kotlin/no/nav/pensjon/kalkulator/lagring/api/dto/LagreSimuleringSpecDtoV1.kt
@@ -166,8 +166,7 @@ data class LagreMaanedligAlderspensjonDto(
     val kapittel20AndelTeller: Int?,
     val kapittel20Trygdetid: Int?,
     val garantipensjonBeloep: Int?,
-    val garantipensjonsnivaaBeloep: Int?,
-    val garantipensjonSats: Double?,
+    val garantipensjonSats: Int?,
     val garantitilleggBeloep: Int?,
     val grunnbeloep: Int?
 )
